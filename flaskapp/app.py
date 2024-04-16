11d43623c7e8c3e1ffbf861be1c09e95e50167bc
SideeID <sandipratama164026@gmail.com>
2024-04-16 07:11:09 +0700

    Refactor Flask app to use new model and image size

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,38 +1,3 @@
-from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import numpy as np
-import os
-
-app = Flask(__name__)
-model = load_model('model.h5')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    img_file = request.files['image']
-    img_path = '../laravel/public/images/' + img_file.filename
-    img_file.save(img_path)
-
-    # img = image.load_img(img_path, target_size=(224, 224))
-    # img_array = image.img_to_array(img)
-    # img_array = np.expand_dims(img_array, axis=0)
-    
-    img = image.load_img(img_path, target_size=(90, 3), color_mode='grayscale')
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = np.expand_dims(img_array, axis=3)
-
-    predictions = model.predict(img_array)
-    predicted_class = np.argmax(predictions, axis=1)
-    
-    os.remove(img_path)
-
-    return jsonify({'prediction': int(predicted_class)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 # from flask import Flask, request, jsonify
 # from tensorflow.keras.models import load_model
 # from tensorflow.keras.preprocessing import image
@@ -40,7 +5,7 @@ if __name__ == '__main__':
 # import os
 
 # app = Flask(__name__)
-# model = load_model('modelbaru.keras')
+# model = load_model('mymodel.h5')
 
 # @app.route('/predict', methods=['POST'])
 # def predict():
@@ -48,18 +13,53 @@ if __name__ == '__main__':
 #     img_path = '../laravel/public/images/' + img_file.filename
 #     img_file.save(img_path)
 
-#     img = image.load_img(img_path, target_size=(224, 224))
+#     # img = image.load_img(img_path, target_size=(224, 224))
+#     # img_array = image.img_to_array(img)
+#     # img_array = np.expand_dims(img_array, axis=0)
+    
+#     img = image.load_img(img_path, target_size=(90, 3), color_mode='grayscale')
 #     img_array = image.img_to_array(img)
 #     img_array = np.expand_dims(img_array, axis=0)
+#     img_array = np.expand_dims(img_array, axis=3)
 
 #     predictions = model.predict(img_array)
 #     predicted_class = np.argmax(predictions, axis=1)
-#     classes = ['dark', 'green', 'light', 'medium']
-#     predicted_label = classes[int(predicted_class)]
-
+    
 #     os.remove(img_path)
 
-#     return jsonify({'prediction': predicted_label})
+#     return jsonify({'prediction': int(predicted_class)})
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model('mymodel.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    img_file = request.files['image']
+    img_path = '../laravel/public/images/' + img_file.filename
+    img_file.save(img_path)
+
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis=1)
+    classes = ['dark', 'green', 'light', 'medium']
+    predicted_label = classes[int(predicted_class)]
+
+    os.remove(img_path)
+
+    return jsonify({'prediction': predicted_label})
+
+if __name__ == '__main__':
+    app.run(debug=True)
